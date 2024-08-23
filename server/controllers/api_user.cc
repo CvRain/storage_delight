7f3fa89875b85a2d7692566fb47c596ffd6af612
cvrain <cvraindays@outlook.com>
2024-08-23 15:59:53 +0800

    写了一个get_user_by_id接口，今天好累不太想写代码

--- server/controllers/api_user.cc
+++ server/controllers/api_user.cc
@@ -10,85 +10,83 @@
 
 using namespace api;
 
-void User::add_user(const HttpRequestPtr& req,
-	std::function<void(const HttpResponsePtr&)>&& callback,
-	schema::BaseUser&& user_request) {
-	spdlog::info("User::add_user");
+void User::add_user(const HttpRequestPtr &req,
+                    std::function<void(const HttpResponsePtr &)> &&callback,
+                    schema::BaseUser &&user_request) {
+    spdlog::info("User::add_user");
 
-	if (user_request.role == schema::UserRole::TypeNone) {
-		util_delight::PrettyResponse::send_base_response("response is formatted incorrectly", k400BadRequest, callback);
-		return;
-	}
+    if (user_request.role == schema::UserRole::TypeNone) {
+        util_delight::PrettyResponse::send_base_response("response is formatted incorrectly", k400BadRequest, callback);
+        return;
+    }
 
-	//检查user_name是否重复
-	if (SqliteServiceProvider::get_instance().get_sqlite_service().check_user_exist(user_request.user_name)) {
-		util_delight::PrettyResponse::send_base_response("The user_name already exists", k400BadRequest, callback);
-		return;
-	}
-
-
-	//检查role是否为0或者1
-	if (user_request.role != 0 && user_request.role != 1) {
-		util_delight::PrettyResponse::send_base_response("The role must be 0 or 1", k400BadRequest, callback);
-		return;
-	}
-
-	//当role为0时，检查数据库中是否已经存在管理员
-	if (user_request.role == 0 && SqliteServiceProvider::get_instance().get_sqlite_service().get_admin_count() > 0) {
-		util_delight::PrettyResponse::send_base_response("Already exist admin account", k400BadRequest, callback);
-		return;
-	}
+    //检查user_name是否重复
+    if (SqliteServiceProvider::get_instance().get_sqlite_service().check_user_exist(user_request.user_name)) {
+        util_delight::PrettyResponse::send_base_response("The user_name already exists", k400BadRequest, callback);
+        return;
+    }
 
 
-	if (const auto result = SqliteServiceProvider::get_instance()
-		.get_sqlite_service()
-		.add_user(schema::BaseUser{ user_request })) {
-		const auto user_response = std::make_unique<model_delight::UserResponse>();
-		user_response->users.emplace_back(result.value());
-		user_response->m_result = "k201Created";
-		user_response->m_code = k201Created;
-		user_response->m_message = "Add user successfully";
-		callback(HttpResponse::newHttpJsonResponse(user_response->to_json()));
-	}
-	else {
-		util_delight::PrettyResponse::send_base_response("Failed to create user", k500InternalServerError, callback);
-	}
+    //检查role是否为0或者1
+    if (user_request.role != 0 && user_request.role != 1) {
+        util_delight::PrettyResponse::send_base_response("The role must be 0 or 1", k400BadRequest, callback);
+        return;
+    }
+
+    //当role为0时，检查数据库中是否已经存在管理员
+    if (user_request.role == 0 && SqliteServiceProvider::get_instance().get_sqlite_service().get_admin_count() > 0) {
+        util_delight::PrettyResponse::send_base_response("Already exist admin account", k400BadRequest, callback);
+        return;
+    }
+
+
+    if (const auto result = SqliteServiceProvider::get_instance()
+            .get_sqlite_service()
+            .add_user(schema::BaseUser{user_request})) {
+        const auto user_response = std::make_unique<model_delight::UserResponse>();
+        user_response->users.emplace_back(result.value());
+        user_response->m_result = "k201Created";
+        user_response->m_code = k201Created;
+        user_response->m_message = "Add user successfully";
+        callback(HttpResponse::newHttpJsonResponse(user_response->to_json()));
+    } else {
+        util_delight::PrettyResponse::send_base_response("Failed to create user", k500InternalServerError, callback);
+    }
 }
 
 
-void User::login(const HttpRequestPtr& req,
-	std::function<void(const HttpResponsePtr&)>&& callback,
-	schema::BaseUser&& user_request) {
+void User::login(const HttpRequestPtr &req,
+                 std::function<void(const HttpResponsePtr &)> &&callback,
+                 schema::BaseUser &&user_request) {
 
     spdlog::info("User::login {}", user_request.user_name);
 
-	//检查用户是否存在
-	const auto result = SqliteServiceProvider::get_instance()
-		.get_sqlite_service()
-		.check_user_exist(user_request.user_name);
+    //检查用户是否存在
+    const auto result = SqliteServiceProvider::get_instance()
+            .get_sqlite_service()
+            .check_user_exist(user_request.user_name);
 
     spdlog::info("{}", result);
 
-	if (!result) {
-		util_delight::PrettyResponse::send_base_response("User does not exist", k400BadRequest, callback);
+    if (!result) {
+        util_delight::PrettyResponse::send_base_response("User does not exist", k400BadRequest, callback);
         spdlog::info("User::login failed: User does not exist", user_request.user_name);
-		return;
-	}
+        return;
+    }
 
-	const auto user = SqliteServiceProvider::get_instance()
-		.get_sqlite_service()
-		.get_user_by_username(user_request.user_name);
+    const auto user = SqliteServiceProvider::get_instance()
+            .get_sqlite_service()
+            .get_user_by_username(user_request.user_name);
 
 
     //检查密码是否正确
-	if(util_delight::StringEncryption::sha256(user_request.password) != user.value().password)
-	{
-		util_delight::PrettyResponse::send_base_response("Password is incorrect", k400BadRequest, callback);
-		return;
-	}
+    if (util_delight::StringEncryption::sha256(user_request.password) != user.value().password) {
+        util_delight::PrettyResponse::send_base_response("Password is incorrect", k400BadRequest, callback);
+        return;
+    }
 
     //返回jwt token
-	Json::Value header;
+    Json::Value header;
     header["alg"] = "HS256";
     header["typ"] = "JWT";
 
@@ -105,7 +103,7 @@ void User::login(const HttpRequestPtr& req,
             header.toStyledString(),
             payload.toStyledString(),
             util_delight::StringEncryption::secret_string
-            );
+    );
 
     Json::Value json_response{};
     json_response["code"] = k200OK;
@@ -118,10 +116,19 @@ void User::login(const HttpRequestPtr& req,
 
 void User::get_user_by_id(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback) {
     spdlog::info("Enter User::get_user_by_id");
-    const auto response = model_delight::CommonResponse()
-            .append("code", k200OK)
-            .append("message", "Get user successfully")
-            .append("func", "get_user_by_id")
-            .to_json();
-    callback(HttpResponse::newHttpJsonResponse(response));
+    const auto required_id = req->getParameter("user_id");
+    if (required_id.empty()) {
+        util_delight::PrettyResponse::send_base_response("user_id is required", k400BadRequest, callback);
+        return;
+    }
+    const auto user = SqliteServiceProvider::get_instance()
+            .get_sqlite_service()
+            .get_user_by_id(std::stoi(required_id));
+
+    auto json_response = std::make_unique<model_delight::UserResponse>();
+    json_response->add_user(user.value())
+    .set_code(user.has_value()? k200OK : k404NotFound)
+    .set_message(user.has_value()? "Get user successfully" : "User does not exist")
+    .set_result(user.has_value()? "k200OK" : "k404NotFound");
+    callback(HttpResponse::newHttpJsonResponse(json_response->to_json()));
 }
