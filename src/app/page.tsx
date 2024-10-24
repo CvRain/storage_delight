d8d09ea677f9787ec9bab09c21fb1eeacb0077f4
strep777 <790955988@qq.com>
2024-10-24 14:01:13 +0800

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import Link from 'next/link'
 
 const App: React.FC = () => (
-    
+    <div className="index">
+        <div className="index-"></div>
+    </div>
 );
 
 export default App;
--- src/app/ui/storageDelight/page.tsx
+++ src/app/ui/storageDelight/page.tsx
@@ -2,7 +2,7 @@ import Client from "./client";
 export default function Page() {
   return (
     <div className="storageDelight-index">
-      
+      <div className="storageDelight-index-"></div>
     </div>
   );
 }
