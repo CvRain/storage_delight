2d9f03ad28fe8722bce9a4d9b272260f46a60726
strep777 <790955988@qq.com>
2024-11-19 13:43:11 +0800

    需要测试

--- src/app/storageDelight/login/page.tsx
+++ src/app/storageDelight/login/page.tsx
@@ -4,18 +4,11 @@ import alovaInstance from '@ut/alova';
 import LoginForm from './loginForm';
 import "./page.css"
 
-const fetchData = async () => {
-  const response = await alovaInstance.Get('/hello/say');
-  return response
-};
-
 const Page:React.FC = () => {
 
   return (
     <div className="storageDelight-login">
-        <div className="storageDelight-login-frame"> 
-            <LoginForm></LoginForm>
-        </div>
+        <LoginForm></LoginForm>
     </div>
   );
 };
